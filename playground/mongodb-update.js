6c37a7a4e7820b629eec8e7b714d8d41d78d3b8b
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-08-16 15:38:40 -0400

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,52 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect(
+  'mongodb://localhost:27017/TodoApp',
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) {
+      return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // findOneAndUpdate
+    // db.collection('Todos')
+    //   .findOneAndUpdate(
+    //     { _id: new ObjectID('5b75c9df79cfc0b1fc0e7ed4') },
+    //     {
+    //       $set: {
+    //         completed: true
+    //       }
+    //     },
+    //     {
+    //       returnOriginal: false
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    db.collection('Users')
+      .findOneAndUpdate(
+        { _id: new ObjectID('5b75b853c3cb5ae6618c2a16') },
+        {
+          $set: {
+            name: 'Alonso'
+          },
+          $inc: {
+            age: 1
+          }
+        },
+        {
+          returnOriginal: false
+        }
+      )
+      .then(result => {
+        console.log(result);
+      });
+
+    // client.close();
+  }
+);
